a038006219cd08e8116079e4f6e3b020a9b0f4be
Rohit joshi <bhdream0000@gmail.com>
2023-11-26 18:22:11 +0530

    removed unwanted elements

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import ContentMain from "./components/Contentmain/ContentMain";
 import SignIn from "./components/User/SignIn";
 import SignUp from "./components/User/SignUp";
 import React from "react";
-import StickyHeadTable from "./components/Table/StickyHeadTable";
+
 import OptimizePortpolio from "./components/Pages/OptimizePortpolio/OptimizePortpolio";
-import Info from "./components/Pages/Info/Info";
+
 function App() {
   return (
     <React.StrictMode>
@@ -35,9 +35,6 @@ function Structure() {
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/" element={<ContentMain />} />
-      <Route path="/home" element={<StickyHeadTable />} />
-      <Route path="/optimize" element={<OptimizePortpolio />} />
-      <Route path="/info" element={<Info />} />
     </Routes>
   </div></>
 }
